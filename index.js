b03fd7ec79b5cabfb0b8737781d3869624816d14
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2023-02-22 22:56:14 +0300

    Add initial JS.

---
+++ index.js
@@ -0,0 +1,16 @@
+var playerScore = 0;
+var botScore = 0;
+
+function play(moveType) {
+  switch (moveType) {
+    case "Rock":
+      console.log("Rock");
+      break;
+    case "Paper":
+      console.log("Paper");
+      break;
+    case "Scissors":
+      console.log("Scissors");
+      break;
+  }
+}
