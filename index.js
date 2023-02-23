f110a0a424d7bec13515f04834e73922e71d0dd9
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2023-02-23 14:33:35 +0300

    Add compuer choosing random choice feature.

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 var playerScore = 0;
 var botScore = 0;
 
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+
 function play(moveType) {
-  switch (moveType) {
-    case "Rock":
-      console.log("Rock");
-      break;
-    case "Paper":
-      console.log("Paper");
-      break;
-    case "Scissors":
-      console.log("Scissors");
-      break;
+  if (typeof moveType !== "string") {
+    console.error("Error with paramter type");
+    return;
   }
+
+  const winning = new Map([
+    ["Paper", "Rock"],
+    ["Scissors", "Paper"],
+    ["Rock", "Scissors"],
+  ]);
+
+  const moves = ["Rock", "Paper", "Scissors"]
+
+  var computerChoice = moves[getRandomInt(0,2)];
+
+  console.log(computerChoice);
 }
