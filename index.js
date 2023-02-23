b2980581011e307f00fd1c6fb09a0d3e711a6a09
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2023-02-23 14:52:41 +0300

    Do the game with player only choosing paper.

--- index.js
+++ index.js
@@ -1,29 +1,32 @@
-var playerScore = 0;
-var botScore = 0;
+let playerScore = 0;
+let botScore = 0;
 
-
+// The maximum is exclusive and the minimum is inclusive
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min) + min); 
 }
 
-
-function play(moveType) {
-  if (typeof moveType !== "string") {
-    console.error("Error with paramter type");
-    return;
-  }
-
+function play(playerMove) {
   const winning = new Map([
     ["Paper", "Rock"],
     ["Scissors", "Paper"],
     ["Rock", "Scissors"],
   ]);
 
-  const moves = ["Rock", "Paper", "Scissors"]
+  const moves = ["Rock", "Paper", "Scissors"];
+    
+  if (typeof playerMove !== 'string' || !moves.includes(playerMove)) {
+    console.error("Error with paramter type");
+    return;
+  }
 
-  var computerChoice = moves[getRandomInt(0,2)];
 
-  console.log(computerChoice);
+  let computerChoice = moves[getRandomInt(0, 3)];
+  
+  if (winning.get(computerChoice) === playerMove) botScore++;
+  else if (winning.get(playerMove) === computerChoice) playerScore++;
+
+  console.log(`Player Score: ${playerScore}, Computer Score: ${botScore}`);
 }
