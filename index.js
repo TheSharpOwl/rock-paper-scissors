23990dd0378c621d68245688e458e036c76279c7
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2023-02-23 16:57:18 +0300

    Make the game play using prompt and total of 5 rounds.

--- index.js
+++ index.js
@@ -1,32 +1,56 @@
-let playerScore = 0;
-let botScore = 0;
+const moves = ["Rock", "Paper", "Scissors"];
 
 // The maximum is exclusive and the minimum is inclusive
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); 
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
-function play(playerMove) {
+function playGame() {
+  let playerScore = 0;
+  let botScore = 0;
+  let result = "";
+
+  for (let i = 0; i < 5; i++) {
+    let message =
+      (result === "Draw" ? result : "") +
+      `\n Your Score ${playerScore}, Computer Score: ${botScore}, Rounds Left: ${
+        5 - i
+      } \n please choose a move`;
+
+    let playerMove = prompt(message);
+
+    if (typeof playerMove !== "string" || !moves.includes(playerMove)) {
+      result = "Error in the value please try again";
+      i--;
+      continue;
+    }
+
+    result = playRound(playerMove);
+    if (result === "PC") botScore++;
+    else if (result === "Player") playerScore++;
+    else result = `${result} last time!`;
+  }
+
+  if (playerScore > botScore) {
+    window.alert("You WIN!");
+  } else if (playerScore == botScore) {
+    window.alert("Draw");
+  } else {
+    window.alert("You Lose :-(");
+  }
+}
+function playRound(playerMove) {
   const winning = new Map([
     ["Paper", "Rock"],
     ["Scissors", "Paper"],
     ["Rock", "Scissors"],
   ]);
 
-  const moves = ["Rock", "Paper", "Scissors"];
-    
-  if (typeof playerMove !== 'string' || !moves.includes(playerMove)) {
-    console.error("Error with paramter type");
-    return;
-  }
-
-
   let computerChoice = moves[getRandomInt(0, 3)];
-  
-  if (winning.get(computerChoice) === playerMove) botScore++;
-  else if (winning.get(playerMove) === computerChoice) playerScore++;
 
-  console.log(`Player Score: ${playerScore}, Computer Score: ${botScore}`);
+  if (winning.get(computerChoice) === playerMove) return "Player";
+  else if (winning.get(playerMove) === computerChoice) return "PC";
+  else return "Draw";
 }
